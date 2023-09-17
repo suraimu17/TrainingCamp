bbc47f0fcfbda6227338ad916d44fb7409c3d4ba
Kazuki Tanida <20RD119@ms.dendai.ac.jp>
2023-09-17 22:18:01 +0900

    add:Rでリトライ

--- Assets/Scripts/GameFlow/GoalController.cs
+++ Assets/Scripts/GameFlow/GoalController.cs
@@ -1,6 +1,8 @@
 using System;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameFlow
 {
@@ -9,7 +11,16 @@ namespace GameFlow
         private Subject<Unit> onTrigerEnterSubject = new Subject<Unit>();
         public IObservable<Unit> OnTriggerEnterObservable => onTrigerEnterSubject;
         public Vector3 GoalPosition => transform.position;
-        
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                var activeSceneName = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(activeSceneName);
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if(collision.gameObject.TryGetComponent<PlayerController>(out _))
