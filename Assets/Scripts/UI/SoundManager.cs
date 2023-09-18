1dc323bd202d9cf8b33d3b5074ac9db299b27183
Ibuki26 <110217043+Ibuki26@users.noreply.github.com>
2023-09-18 10:02:41 +0900

    SoundManagerの改良

--- Assets/Scripts/UI/SoundManager.cs
+++ Assets/Scripts/UI/SoundManager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using System;
 
 public class SoundManager : SingletonMonoBehaviour<SoundManager>
 {
-    [SerializeField] AudioClip buttonSE;
+    [SerializeField] private List<AudioData> audioDataList;
 
-    public AudioSource audioSource;
+    public AudioSource audioSource { get; private set; }
     static public float soundVolume = 1f;
 
     // Start is called before the first frame update
@@ -17,8 +17,25 @@ public class SoundManager : SingletonMonoBehaviour<SoundManager>
         audioSource.volume = soundVolume;
     }
 
-    public void PlaySE()
+    public void PlaySE(AudioType type)
     {
-        audioSource.PlayOneShot(buttonSE);
+        AudioClip audioClip = FindAudioClip(type);
+        audioSource.PlayOneShot(audioClip);
+    }
+
+    private AudioClip FindAudioClip(AudioType type)
+    {
+        var data = audioDataList.Find(x => x.Type == type);
+        if(data is null)
+        {
+            throw new Exception($"Audio data is null. (type: {type})");
+        }
+
+        if(data.Clip is null)
+        {
+            throw new Exception($"Audio clip is null. (type: {type})");
+        }
+
+        return data.Clip;
     }
 }
